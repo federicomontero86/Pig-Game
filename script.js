1207b796dccd96544a9af80f1e9c8e8e5a2c466c
federicomontero86 <104212036+federicomontero86@users.noreply.github.com>
2022-09-21 17:33:52 -0300

    Starting Conditions
    - Total Scores changed to 0
    - No Dice visible

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+// select element by its ID
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
