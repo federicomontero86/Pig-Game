5d3eda23dc98287a0067f4d4502842966792aa84
federicomontero86 <104212036+federicomontero86@users.noreply.github.com>
2022-09-22 10:17:08 -0300

    New Features
    - Update Current Scores
    - When the Dice rolls 1 Current Score changes to 0 and Switch the Player
    - Update Background Color for the Active Player

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 // select element by its ID
 const score1El = document.getElementById('score--1');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
